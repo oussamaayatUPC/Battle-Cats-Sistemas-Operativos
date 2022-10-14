872bfa30512a98c76e6649620aadc2c0883f7a47
oussamaayatUPC <oussama.ayat@estudiantat.upc.edu>
2022-10-14 10:17:31 +0200

    Versión 1 generada por Oussama Ayat

---
+++ Cliente/Cliente/Formulario_de_bienvenida.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Cliente
+{
+    partial class Formulario_de_bienvenida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texto_bienvenida = new System.Windows.Forms.Label();
+            this.consultar_partidas_dia = new System.Windows.Forms.Button();
+            this.victorias_button = new System.Windows.Forms.Button();
+            this.tiempo_medio_button = new System.Windows.Forms.Button();
+            this.dia_textbox = new System.Windows.Forms.TextBox();
+            this.etiqueta_dia = new System.Windows.Forms.Label();
+            this.usuarios_grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.usuarios_grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // texto_bienvenida
+            // 
+            this.texto_bienvenida.AutoSize = true;
+            this.texto_bienvenida.Location = new System.Drawing.Point(314, 13);
+            this.texto_bienvenida.Name = "texto_bienvenida";
+            this.texto_bienvenida.Size = new System.Drawing.Size(71, 13);
+            this.texto_bienvenida.TabIndex = 0;
+            this.texto_bienvenida.Text = "Bienvenido/a";
+            this.texto_bienvenida.Click += new System.EventHandler(this.texto_bienvenida_Click);
+            // 
+            // consultar_partidas_dia
+            // 
+            this.consultar_partidas_dia.Location = new System.Drawing.Point(40, 75);
+            this.consultar_partidas_dia.Name = "consultar_partidas_dia";
+            this.consultar_partidas_dia.Size = new System.Drawing.Size(169, 61);
+            this.consultar_partidas_dia.TabIndex = 1;
+            this.consultar_partidas_dia.Text = "Consultar número de partidas jugadas en X día.";
+            this.consultar_partidas_dia.UseVisualStyleBackColor = true;
+            this.consultar_partidas_dia.Click += new System.EventHandler(this.consultar_partidas_dia_Click);
+            // 
+            // victorias_button
+            // 
+            this.victorias_button.Location = new System.Drawing.Point(40, 154);
+            this.victorias_button.Name = "victorias_button";
+            this.victorias_button.Size = new System.Drawing.Size(169, 52);
+            this.victorias_button.TabIndex = 2;
+            this.victorias_button.Text = "Número de victorias de un jugador";
+            this.victorias_button.UseVisualStyleBackColor = true;
+            this.victorias_button.Click += new System.EventHandler(this.victorias_button_Click);
+            // 
+            // tiempo_medio_button
+            // 
+            this.tiempo_medio_button.Location = new System.Drawing.Point(40, 232);
+            this.tiempo_medio_button.Name = "tiempo_medio_button";
+            this.tiempo_medio_button.Size = new System.Drawing.Size(169, 61);
+            this.tiempo_medio_button.TabIndex = 3;
+            this.tiempo_medio_button.Text = "Tiempo medio de partidas de un jugador";
+            this.tiempo_medio_button.UseVisualStyleBackColor = true;
+            this.tiempo_medio_button.Click += new System.EventHandler(this.tiempo_medio_button_Click);
+            // 
+            // dia_textbox
+            // 
+            this.dia_textbox.Location = new System.Drawing.Point(377, 96);
+            this.dia_textbox.Name = "dia_textbox";
+            this.dia_textbox.Size = new System.Drawing.Size(254, 20);
+            this.dia_textbox.TabIndex = 4;
+            // 
+            // etiqueta_dia
+            // 
+            this.etiqueta_dia.AutoSize = true;
+            this.etiqueta_dia.Location = new System.Drawing.Point(377, 77);
+            this.etiqueta_dia.Name = "etiqueta_dia";
+            this.etiqueta_dia.Size = new System.Drawing.Size(211, 13);
+            this.etiqueta_dia.TabIndex = 5;
+            this.etiqueta_dia.Text = "Escribe un día en el formato \"aaaa-mm-dd\"";
+            // 
+            // usuarios_grid
+            // 
+            this.usuarios_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usuarios_grid.Location = new System.Drawing.Point(378, 123);
+            this.usuarios_grid.Name = "usuarios_grid";
+            this.usuarios_grid.Size = new System.Drawing.Size(254, 170);
+            this.usuarios_grid.TabIndex = 8;
+            // 
+            // Formulario_de_bienvenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.usuarios_grid);
+            this.Controls.Add(this.etiqueta_dia);
+            this.Controls.Add(this.dia_textbox);
+            this.Controls.Add(this.tiempo_medio_button);
+            this.Controls.Add(this.victorias_button);
+            this.Controls.Add(this.consultar_partidas_dia);
+            this.Controls.Add(this.texto_bienvenida);
+            this.Name = "Formulario_de_bienvenida";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Formulario_de_bienvenida_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usuarios_grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label texto_bienvenida;
+        private System.Windows.Forms.Button consultar_partidas_dia;
+        private System.Windows.Forms.Button victorias_button;
+        private System.Windows.Forms.Button tiempo_medio_button;
+        private System.Windows.Forms.TextBox dia_textbox;
+        private System.Windows.Forms.Label etiqueta_dia;
+        private System.Windows.Forms.DataGridView usuarios_grid;
+    }
+}
---
+++ Cliente/Cliente/Login.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Cliente
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inicio_de_sesion = new System.Windows.Forms.Button();
+            this.nombre_usuario = new System.Windows.Forms.TextBox();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.etiqueta_usuario = new System.Windows.Forms.Label();
+            this.etiqueta_contraseña = new System.Windows.Forms.Label();
+            this.etiqueta_mensaje_de_error = new System.Windows.Forms.Label();
+            this.registro = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // inicio_de_sesion
+            // 
+            this.inicio_de_sesion.Location = new System.Drawing.Point(308, 264);
+            this.inicio_de_sesion.Name = "inicio_de_sesion";
+            this.inicio_de_sesion.Size = new System.Drawing.Size(127, 28);
+            this.inicio_de_sesion.TabIndex = 0;
+            this.inicio_de_sesion.Text = "Inicio de sesión";
+            this.inicio_de_sesion.UseVisualStyleBackColor = true;
+            this.inicio_de_sesion.Click += new System.EventHandler(this.inicio_de_sesion_Click);
+            // 
+            // nombre_usuario
+            // 
+            this.nombre_usuario.Location = new System.Drawing.Point(321, 158);
+            this.nombre_usuario.Name = "nombre_usuario";
+            this.nombre_usuario.Size = new System.Drawing.Size(100, 20);
+            this.nombre_usuario.TabIndex = 1;
+            this.nombre_usuario.TextChanged += new System.EventHandler(this.nombre_usuario_TextChanged);
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(321, 207);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(100, 20);
+            this.contraseña.TabIndex = 2;
+            // 
+            // etiqueta_usuario
+            // 
+            this.etiqueta_usuario.AutoSize = true;
+            this.etiqueta_usuario.Location = new System.Drawing.Point(321, 139);
+            this.etiqueta_usuario.Name = "etiqueta_usuario";
+            this.etiqueta_usuario.Size = new System.Drawing.Size(96, 13);
+            this.etiqueta_usuario.TabIndex = 3;
+            this.etiqueta_usuario.Text = "Nombre de usuario";
+            // 
+            // etiqueta_contraseña
+            // 
+            this.etiqueta_contraseña.AutoSize = true;
+            this.etiqueta_contraseña.Location = new System.Drawing.Point(321, 185);
+            this.etiqueta_contraseña.Name = "etiqueta_contraseña";
+            this.etiqueta_contraseña.Size = new System.Drawing.Size(61, 13);
+            this.etiqueta_contraseña.TabIndex = 4;
+            this.etiqueta_contraseña.Text = "Contraseña";
+            // 
+            // etiqueta_mensaje_de_error
+            // 
+            this.etiqueta_mensaje_de_error.AutoSize = true;
+            this.etiqueta_mensaje_de_error.Location = new System.Drawing.Point(291, 236);
+            this.etiqueta_mensaje_de_error.Name = "etiqueta_mensaje_de_error";
+            this.etiqueta_mensaje_de_error.Size = new System.Drawing.Size(0, 13);
+            this.etiqueta_mensaje_de_error.TabIndex = 5;
+            // 
+            // registro
+            // 
+            this.registro.AutoSize = true;
+            this.registro.Location = new System.Drawing.Point(274, 316);
+            this.registro.Name = "registro";
+            this.registro.Size = new System.Drawing.Size(187, 13);
+            this.registro.TabIndex = 6;
+            this.registro.TabStop = true;
+            this.registro.Text = "¿Aún no estás registrado? Pulse aquí.";
+            this.registro.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.registro_LinkClicked);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.registro);
+            this.Controls.Add(this.etiqueta_mensaje_de_error);
+            this.Controls.Add(this.etiqueta_contraseña);
+            this.Controls.Add(this.etiqueta_usuario);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.nombre_usuario);
+            this.Controls.Add(this.inicio_de_sesion);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inicio_de_sesion;
+        private System.Windows.Forms.TextBox nombre_usuario;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Label etiqueta_usuario;
+        private System.Windows.Forms.Label etiqueta_contraseña;
+        private System.Windows.Forms.Label etiqueta_mensaje_de_error;
+        private System.Windows.Forms.LinkLabel registro;
+    }
+}
+
